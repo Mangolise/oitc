1851585ef6469838a6cf8761035ddadd954abe6c
Calcilore <github@calcilore.net>
2024-08-31 23:03:38 +1000

    Some work in ParticleMenu

--- src/main/java/net/mangolise/oitc/ParticleMenu.java
+++ src/main/java/net/mangolise/oitc/ParticleMenu.java
@@ -1,8 +1,9 @@
 package net.mangolise.oitc;
 
-import net.minestom.server.MinecraftServer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextDecoration;
+import net.mangolise.gamesdk.util.GameSdkUtils;
 import net.minestom.server.color.Color;
-import net.minestom.server.component.DataComponent;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.inventory.InventoryPreClickEvent;
 import net.minestom.server.inventory.Inventory;
@@ -15,20 +16,25 @@ import net.minestom.server.particle.Particle;
 import net.minestom.server.potion.PotionType;
 import net.minestom.server.tag.Tag;
 
-import java.util.Map;
+import java.util.List;
 
 public class ParticleMenu {
-    private static final Map<Color, Particle> particleMap = Map.of(new Color(150, 255, 225), Particle.GLOW);
-    static final Tag<Particle> ARROW_PARTICLE = Tag.Transient("arrow_particle");
+    private static final Tag<Integer> ARROW_PARTICLE = Tag.Integer("arrow_particle");
+    private static final List<ColoredParticle> particles = List.of(
+            new ColoredParticle(new Color(150, 255, 225), Particle.GLOW)
+    );
 
     public static void openMenu(Player player) {
         Inventory inventory = new Inventory(InventoryType.CHEST_4_ROW, "Particle Menu");
 
-        particleMap.forEach((color, particle) -> {
-          ItemStack arrow = ItemStack.of(Material.TIPPED_ARROW).with(ItemComponent.POTION_CONTENTS,
-                    new PotionContents(PotionType.AWKWARD, color)).withTag(ARROW_PARTICLE, particle);
-            inventory.addItemStack(arrow);
-        });
+        for (int i = 0; i < particles.size(); i++) {
+            ColoredParticle coloredParticle = particles.get(i);
+            Particle particle = coloredParticle.particle();
+
+            inventory.addItemStack(ItemStack.of(Material.TIPPED_ARROW).with(ItemComponent.POTION_CONTENTS,
+                    new PotionContents(PotionType.AWKWARD, coloredParticle.color())).withTag(ARROW_PARTICLE, i)
+                    .withCustomName(Component.text(GameSdkUtils.capitaliseFirstLetter(particle.key().value().replace('_', ' '))).decoration(TextDecoration.ITALIC, false)));
+        }
 
         player.openInventory(inventory);
     }
@@ -37,10 +43,12 @@ public class ParticleMenu {
         ItemStack clickedItem = e.getClickedItem();
 
         if (clickedItem.material().equals(Material.TIPPED_ARROW)) {
-            Particle particle = clickedItem.getTag(ARROW_PARTICLE);
+            Particle particle = particles.get(clickedItem.getTag(ARROW_PARTICLE)).particle();
             e.getPlayer().setTag(OITC.PLAYER_ARROW_PARTICLE, particle);
             e.setCancelled(true);
             e.getPlayer().closeInventory();
         }
     }
+
+    private record ColoredParticle(Color color, Particle particle) {}
 }
