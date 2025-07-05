661419e0422a363a9d4a4f8022ebb3cc9a5e4f60
EclipsedMango <140897468+EclipsedMango@users.noreply.github.com>
2025-07-05 23:01:14 +1000

    fixed playerstats head not using profile datacomponent

--- src/main/java/net/mangolise/oitc/menus/StatsMenu.java
+++ src/main/java/net/mangolise/oitc/menus/StatsMenu.java
@@ -6,6 +6,7 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.mangolise.gamesdk.util.ChatUtil;
 import net.mangolise.oitc.OITC;
+import net.minestom.server.component.DataComponents;
 import net.minestom.server.entity.Player;
 import net.minestom.server.inventory.Inventory;
 import net.minestom.server.inventory.InventoryType;
@@ -24,7 +25,7 @@ public class StatsMenu {
                         .append(Component.text(" Stats").decoration(TextDecoration.ITALIC, false).color(NamedTextColor.GOLD)));
 
         if (player.getSkin() != null) {
-            //playerStats = playerStats.with(, new HeadProfile(player.getSkin()));
+            playerStats = playerStats.with(DataComponents.PROFILE, new HeadProfile(player.getSkin()));
         }
 
         Component lineBreak = Component.text("--------------------").decoration(TextDecoration.ITALIC, false).color(NamedTextColor.DARK_GRAY);
